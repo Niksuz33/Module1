49d28d6eca0d942dcd4393b3d5d169f0b9511aeb
Niksuz33 <kostantinnikolaev33@gmail.com>
2024-05-12 18:42:32 +0300

    homework2.py

--- homework2.py
+++ homework2.py
@@ -1,3 +1,6 @@
+# в задании было просто вывести значения, но в примере было с указанием текста типа <Name: John>
+# Поэтому сделал оба варианта
+
 name = 'Konstantin'
 print(name)
 age = 41
@@ -6,3 +9,12 @@ age = age + 1
 print(age)
 is_student = True
 print(is_student)
+
+name = 'Konstantin'
+print('Name: ' + name)
+age = 41
+print('Age: ', age)
+age = age + 1
+print('New age: ', age)
+is_student = True
+print('Is Student: ', is_student)
